f3ae2595e2c18a61a3eca1d43aa5d797fc2495fb
Siddharth Gupta <50405111+ShadowBaby22@users.noreply.github.com>
2023-01-19 22:19:42 +0530

    Edits to header file

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -17,6 +17,12 @@ export const Container = styled.div`
     grid-row-gap: 0.5rem;
   }
 `;
+
+export const Span = styled.span` //backtick is a template literal
+  font-size: 2rem;
+`;
+
+
 export const Div1 = styled.div`
   grid-area: 1 / 1 / 2 / 2;
   display: flex;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,14 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello World <br />
+        Take a minute or 2 to take a look at what I am up to 
+      </SectionTitle>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
