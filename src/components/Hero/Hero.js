baef879656b793422d97528dfb68902dc1ac47a7
Siddharth Gupta <50405111+ShadowBaby22@users.noreply.github.com>
2023-01-24 16:57:35 +0530

    updates

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,15 +4,21 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
+const Hero = () => (
   <Section row nopadding>
     <LeftSection>
       <SectionTitle main center>
         Hello World <br />
         Take a minute or 2 to take a look at what I am up to 
       </SectionTitle>
+      <SectionText>
+        See how I use tech so I can make a difference 🤓
+      </SectionText>
+      <Button onClick={()=> window.location = 'https://google.com'}> Learn More </Button> {/*Onclick has a Callback function using arrow function*/}
     </LeftSection>
-  </Section>
+    </Section>
+  
+
 );
 
 export default Hero;
